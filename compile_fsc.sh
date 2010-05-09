719fdd736597d7aeebcd6199b33aa1f3e1546a88
Sciss <contact@sciss.de>
2010-05-09 21:08:02 +0100

    removing obsolete shell script

--- compile_fsc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Compiling..."
-fsc -deprecation -d out/production/ScalaOSC/ -sourcepath src/ src/de/sciss/scalaosc/*.scala
-echo "Archiving..."
-jar cf ScalaOSC.jar -C out/production/ScalaOSC/ .
-jar uf ScalaOSC.jar -C resources/ .
-echo "Done."
